50155742de636709f7cbfd531d3eac0f9a6fa8b9
elleven11 <federico.cassano@federico.codes>
2023-01-25 22:36:41 -0500

    run fastt

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+# get by $1 the number of jobs to spawn
+if [ -z "$1" ]; then
+  echo "Usage: $0 <number of jobs (more = more speed, too much = crash computer)>"
+    exit 1
+fi
+
 cd build
 cmake -G "Ninja" -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_TARGETS_TO_BUILD="ARM;Lanai;RISCV" -DCMAKE_BUILD_TYPE="Debug" -DLLVM_ENABLE_ASSERTIONS=On ../llvm
-ninja
+ninja -j $1
