fecb7ee098a0be899c9ce6fbc98e8b053366468c
elleven11 <federico.cassano@federico.codes>
2023-01-29 22:37:22 -0500

    intro encryption

--- scripts/build_clangonly.sh
+++ scripts/build_clangonly.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# get by $1 the number of jobs to spawn
-if [ -z "$1" ]; then
-  echo "Usage: $0 <number of jobs (more = more speed, too much = crash computer)>"
-    exit 1
-fi
-
+./scripts/build_common.sh $@
 cd build
-cmake -G "Ninja" -DLLVM_ENABLE_PROJECTS="clang" -DLLVM_TARGETS_TO_BUILD="X86" -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DLLVM_ENABLE_ASSERTIONS=On ../llvm
 ninja -j $1 clang
