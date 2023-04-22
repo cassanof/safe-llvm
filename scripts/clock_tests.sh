47cf061688ba7cd2deb4e69cab37b95ebe2e0495
elleven11 <federico.cassano@federico.codes>
2023-04-21 20:13:39 -0400

    script to time tests

---
+++ scripts/clock_tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# this script runs `make test` $1 times and reports the average time
+# it takes to run the tests, in milliseconds
+
+# run the tests $1 times
+AVG=0
+ITERS=$1
+for i in $(seq 1 $ITERS);
+do
+    echo "Running test $i of $ITERS"
+    # get milliseconds since epoch
+    START=$(date +%s%3N)
+    make test
+    END=$(date +%s%3N)
+    # get the difference in milliseconds
+    DIFF=$(( $END - $START ))
+    AVG=$(( $AVG + $DIFF ))
+done
+
+# divide by the number of iterations to get the average
+AVG=$(( $AVG / $ITERS ))
+echo "Average time to run tests: $AVG milliseconds"
